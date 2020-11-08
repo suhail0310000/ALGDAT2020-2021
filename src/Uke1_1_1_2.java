fe3c0344a2cb8d3ca8743b4d15a44bad333738d3
suhail <suhail_0310@hotmail.com>
2020-11-08 14:21:21 +0100

    min metode - finne posisjon til minste verdi i en tabell i en tabell

--- src/Uke1_1_1_2.java
+++ src/Uke1_1_1_2.java
@@ -1,7 +1,17 @@
 public class Uke1_1_1_2 {
     public static void main(String[] args){
-        for(int i = 0; i<10; i++){
-            System.out.print(i); //skriver ut tall fra 0-9
+        int[] a = {9,8,7,3,2,1};
+        System.out.print(min(a));
+    }
+    public static int min(int[] a){
+        //finner minste verdi
+        int min_index = 0;
+        for(int i = 0; i<a.length; i++){
+            if(a[i]<a[min_index]){
+                min_index = i; //indeksen oppdateres
+            }
         }
+        return min_index; //returnerer indeksen
     }
 }
+
